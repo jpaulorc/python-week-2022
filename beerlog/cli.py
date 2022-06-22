dad529c0c7212407dd31d7339a6d553719465609
Joao Corte <jpaulorc@gmail.com>
2022-06-22 19:45:09 -0300

    Second Day - Python Week

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,6 +1,13 @@
+from typing import Optional
+
 import typer
+from rich.console import Console
+from rich.table import Table
+
+from beerlog.core import add_beer_to_database, get_beers_from_database
 
 main = typer.Typer(help="Beer Management Application")
+console = Console()
 
 
 @main.command("add")
@@ -17,14 +24,26 @@ def add_beer(
         name (str): Beer Name
         style (str): Beer Style
     """
-    print(name, style)
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print("🍺 beer added to database")
 
 
 @main.command("list")
-def list_beers(style: str):
+def list_beers(style: Optional[str] = None):
     """Lists beers in database
 
     Args:
         style (str): Beer Style
     """
-    print(style)
+    beers = get_beers_from_database(style)
+    table = Table(title="Brewlog :beer_mug:")
+    headers = ["id", "name", "style", "rate", "date"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+
+    for beer in beers:
+        beer.date = beer.date.strftime("%Y-%m-%d")
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+
+    console.print(table)
--- beerlog/database.py
+++ beerlog/database.py
@@ -1,12 +1,16 @@
-from sqlmodel import create_engine
+import warnings
+
+from sqlalchemy.exc import SAWarning
+from sqlmodel import Session, create_engine
 
 from beerlog import models
 from beerlog.config import settings
 
-engine = create_engine(settings.database.url)
+warnings.filterwarnings("ignore", category=SAWarning)
 
+engine = create_engine(settings.database.url, echo=False)
 models.SQLModel.metadata.create_all(engine)
 
 
 def get_session():
-    return Session(session)
+    return Session(engine)
